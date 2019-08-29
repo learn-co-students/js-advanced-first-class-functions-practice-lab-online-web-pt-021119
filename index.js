2cf69864e72425da5ea6cafae7363b197084c1da
cyantis <cyantis@gmail.com>
2019-08-29 08:44:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function logDriverNames(arr){
+  arr.forEach(function (el, i, arr){
+    console.log(el.name);
+  });
+}
+
+function logDriversByHometown(arr, town){
+  arr.forEach(function (el, i, arr){
+    if(el.hometown === town){
+      console.log(el.name);
+    }
+  });
+}
+
+function driversByRevenue(arr){
+  const newArr = [...arr].sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+  return newArr;
+}
