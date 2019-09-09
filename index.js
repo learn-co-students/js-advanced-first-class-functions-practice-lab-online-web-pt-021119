464a8597a88f6e92ce6bf274d800b1bf752bb424
lieby weintraub <liebahinda@gmail.com>
2019-09-09 15:11:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const logDriverNames= function(drivers) {
+ return  drivers.forEach(function(element) {
+    console.log(element.name);
+  });
+};
+
+const logDriversByHometown= function (drivers, hometown) { drivers.forEach( function(element) {
+  if (element.hometown === hometown) { console.log(element.name);
+  }
+});
+}
+
+const driversByRevenue= function(drivers){
+  return drivers.slice().sort(function(a, b) {
+    return a.revenue- b.revenue
+    
+  });
+}
+
+const driversByName= function(drivers){
+  return drivers.slice().sort(function(a,b){ return  a.name .localeCompare(b.name);
+    
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function(total, driver) {
+    return total + driver.revenue;
+  }, 0);
+};
+
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
